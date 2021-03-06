7f60a16f9f37d0a5097b5558397716ad6cc9ba16
Hito <413168358@qq.com>
2021-03-06 21:59:27 +0800

    add ad hit me button on the gui

--- Autogui.py
+++ Autogui.py
@@ -2,6 +2,19 @@ import tkinter as tk
 window=tk.Tk()
 window.title('my window')
 window.geometry('400x100')
-l=tk.Label(window,text='OMG!this is TK',bg='green',font=('Arial',12),width=15,height=2)
+var = tk.StringVar()
+l=tk.Label(window,textvariable=var,bg='green',font=('Arial',12),width=15,height=2)
 l.pack()
+global on_hit
+on_hit = False
+def hit_me():
+    global on_hit
+    if on_hit==False:
+        on_hit=True
+        var.set('you hit me')
+    else:
+        on_hit=False
+        var.set('')
+b=tk.Button(window, text='hit me', width=15, height=2, command=hit_me)
+b.pack()
 window.mainloop()
