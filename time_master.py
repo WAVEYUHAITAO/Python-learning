cda18ed5c658b7324bd36bb26a119b64a3fda6aa
hito <413168358@qq.com>
2022-10-01 12:37:56 +0800

    1

--- time_master.py
+++ time_master.py
@@ -1,13 +1,15 @@
 import time
+def time_master(func):
+    def call_func():
+        print("start the function~")
+        start=time.time()
+        func()
+        stop=time.time()
+        print("stop the function")
+        print(f"The total time consumed:{(stop-start):.2f}s")
+    return call_func
+@time_master #装饰器
 def myfunc():
     time.sleep(2)
     print("Hello HAITAO!")
-def time_master(func):
-    print("start the function~")
-    start=time.time()
-    func()
-    stop=time.time()
-    print("stop the function")
-    print(f"The total time consumed:{(stop-start):.2f}s")
-
-time_master(myfunc)
+myfunc()
