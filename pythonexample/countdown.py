e99731e7eba11e13511821db5ae788dfa7960b7c
hito <413168358@qq.com>
2023-10-10 11:27:55 +0800

    qwe

--- pythonexample/countdown.py
+++ pythonexample/countdown.py
@@ -3,11 +3,11 @@
 
 import time, subprocess
 
-timeLeft = 60
+timeLeft = 5
 while timeLeft > 0:
     print(timeLeft, end='')
     time.sleep(1)
     timeLeft = timeLeft - 1
 
 # At the end of the countdown, play a sound file.
-subprocess.Popen(['start','alarm.wav'], shell=True)
+subprocess.Popen(['start','E:\Repositories\Python-learning\pythonexample\\alarm.wav'], shell=True)
