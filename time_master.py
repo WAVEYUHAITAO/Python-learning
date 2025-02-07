3ca683d73e0629ad658f2e7a740c2e8c142eacf5
haitao <413168358@qq.com>
2025-02-07 17:26:42 +0800

    更改格式

--- time_master.py
+++ time_master.py
@@ -1,15 +1,22 @@
 import time
+
+
 def time_master(func):
     def call_func():
         print("start the function~")
-        start=time.time()
+        start = time.time()
         func()
-        stop=time.time()
+        stop = time.time()
         print("stop the function")
-        print(f"The total time consumed:{(stop-start):.2f}s")
+        print(f"The total time consumed:{(stop - start):.2f}s")
+
     return call_func
-@time_master #装饰器
+
+
+@time_master  # 装饰器
 def myfunc():
     time.sleep(2)
     print("Hello HAITAO!")
-myfunc() 
+
+
+myfunc()
