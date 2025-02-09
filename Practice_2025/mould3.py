b59c9fbf3e41c4a9e52e4a0b1da0e45188dc4f79
haitao <413168358@qq.com>
2025-02-09 21:55:22 +0800

    练习用

---
+++ Practice_2025/mould3.py
@@ -0,0 +1,19 @@
+def foo():
+    print("hello world")
+    pass
+
+def bar():
+    print("good night")
+    pass
+
+if __name__=='__main__':
+    print('call foo()')
+    print('call bar()')
+    foo()
+    bar()
+
+
+
+
+
+
